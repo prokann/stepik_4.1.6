db04de0479c1ef2798db4c70a5c30c5c335d1b8c
prokann <annaaudia5@gmail.com>
2020-12-25 13:20:00 +0200

    Task 4.3.13

--- pages/base_page.py
+++ pages/base_page.py
@@ -16,7 +16,7 @@ class BasePage:
         self.browser.get(self.url)
 
     def go_to_login_page(self):
-        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK_INVALID)
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
         link.click()
 
     def go_to_cart_page(self):
@@ -25,6 +25,10 @@ class BasePage:
 
     def should_be_login_link(self):
         assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), \
+            "User icon is not presented, probably unauthorised user"
 
     def is_element_present(self, how, what):
         try:
--- test_product_page.py
+++ test_product_page.py
@@ -18,6 +18,34 @@ urls[7] = pytest.param("7", marks=pytest.mark.xfail)
 #     cart.solve_quiz_and_get_code()
 #     cart.price_correct()
 #     cart.items_correct()
+@pytest.mark.user_register
+class TestUserAddToCartFromProductPage(object):
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        login_page = LoginPage(browser, link)
+        login_page.open()
+        login_page.go_to_login_page()
+        email = str(time.time()) + "@fakemail.org"
+        password = "159753qawsedrftg"
+        login_page.register_new_user(email, password)
+        login_page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+        cart = ProductPage(browser, link)
+        cart.open()
+        cart.should_not_be_success_message()
+
+    def test_user_can_add_product_to_cart(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+        cart = ProductPage(browser, link)
+        cart.open()
+        cart.add_to_cart()
+        cart.price_correct()
+        cart.items_correct()
+
+
 @pytest.mark.login_link
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
@@ -43,13 +71,6 @@ def test_guest_cant_see_success_message_after_adding_product_to_cart(browser):
     cart.should_not_be_success_message()
 
 
-def test_user_cant_see_success_message(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
-    cart = ProductPage(browser, link)
-    cart.open()
-    cart.should_not_be_success_message()
-
-
 def test_message_disappeared_after_adding_product_to_cart(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/hacking-exposed-wireless_208/"
     cart = ProductPage(browser, link)
@@ -66,3 +87,5 @@ def test_guest_cant_see_product_in_cart_opened_from_product_page(browser):
     page.go_to_cart_page()
     c_page = CartPage(browser, browser.current_url)
     c_page.cart_page()
+
+
